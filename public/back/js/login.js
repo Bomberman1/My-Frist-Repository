c5693316f7dcfc70534c3bb77a8f4f3f7b60dc85
unknown <17621245592@163.com>
2018-12-14 19:38:51 +0800

    登录功能完成

---
+++ public/back/js/login.js
@@ -0,0 +1,36 @@
+$(function () {
+    $('#form').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: '用户名不能为空'
+                    },
+                    stringLength: {
+                        min: 2,
+                        max: 6,
+                        message: '用户名必须是2-6位之间'
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: '密码不能为空'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: '密码必须是6-30位之间'
+                    }
+                }
+            }
+            
+        }
+    })
+})
